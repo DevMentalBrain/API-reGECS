f1c102dba1027e0b47720f7b31da0ec1e01bc4e3
Cauã Mendes <cauamendes.dev@gmail.com>
2026-06-07 21:51:19 -0300

    Add: função que valida o aluno que pode subir de ano e que gera os movimentos do professor.

--- app/logic.py
+++ app/logic.py
@@ -37,7 +37,7 @@ def get_my_professors(state):
 
     return professors
 
-#Função que valida os proximos passos do professor
+#Função que valida os proximos passos possiveis do professor
 def get_valid_moves_for_professor(state, row, col):
 
     valid_moves = []
@@ -73,11 +73,115 @@ def get_valid_moves_for_professor(state, row, col):
 
     return valid_moves
 
+#Função que valida qual o aluno que o professor pode subir de ano
+def get_valid_mentors(state, row, col):
 
-# Função que gera as proximas jogadas
-def generate_moves(state) -> list[Move]:
+    mentors = []
+
+    directions = [
+        (-1, -1), (-1, 0), (-1, 1),
+        ( 0, -1),          ( 0, 1),
+        ( 1, -1), ( 1, 0), ( 1, 1)
+    ]
+
+    for dr, dc in directions:
+
+        mentor_row = row + dr
+        mentor_col = col + dc
+
+        if not (0 <= mentor_row < 5 and 0 <= mentor_col < 5):
+            continue
+
+        cell = state.board[mentor_row][mentor_col]
+
+        # Não pode ter professor
+        if cell.professor is not None:
+            continue
+
+        # Não pode ser graduado
+        if cell.level == 4:
+            continue
+
+        mentors.append(
+            Position(
+                row=mentor_row,
+                col=mentor_col
+            )
+        )
+
+    return mentors
+
+#Função para ter todos os movimentos possiveis do professor
+def generate_moves(state):
+
     moves = []
 
-    # vamos implementar depois
+    professors = get_my_professors(state)
+
+    for professor in professors:
+
+        valid_positions = get_valid_moves_for_professor(
+            state,
+            professor["row"],
+            professor["col"]
+        )
+
+        for position in valid_positions:
+
+            mentors = get_valid_mentors(
+                state,
+                position.row,
+                position.col
+            )
+
+            for mentor in mentors:
+
+                moves.append(
+                    Move(
+                        professor=professor["name"],
+                        move_to=position,
+                        mentor_at=mentor
+                    )
+                )
 
     return moves
+
+#Proximo movimento do professor, contendo para onde ele foi e qual aluno subiu
+def apply_move(state, move):
+
+    new_state = deepcopy(state)
+
+    old_row = None
+    old_col = None
+
+    # encontra professor
+    for row in range(5):
+        for col in range(5):
+
+            cell = new_state.board[row][col]
+
+            if cell.professor == move.professor:
+                old_row = row
+                old_col = col
+                break
+
+    # remove da posição antiga
+    new_state.board[old_row][old_col].professor = None
+
+    # coloca na nova posição
+    new_state.board[
+        move.move_to.row
+    ][
+        move.move_to.col
+    ].professor = move.professor
+
+    # aplica mentoria
+    mentor_cell = new_state.board[
+        move.mentor_at.row
+    ][
+        move.mentor_at.col
+    ]
+
+    mentor_cell.level += 1
+
+    return new_state
